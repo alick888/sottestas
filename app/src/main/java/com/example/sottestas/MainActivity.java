e73c58ab85fa56a0779033619fa541bd8b791e15
alick888 <alick_w@yahoo.com.hk>
2021-04-09 23:33:20 +0800

    Androidx

--- app/src/main/java/com/example/sottestas/MainActivity.java
+++ app/src/main/java/com/example/sottestas/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.sottestas;
 
-import android.content.Context;
+//import android.content.Context;
 import android.os.Bundle;
-import android.os.Environment;
-import android.support.constraint.ConstraintLayout;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+//import android.os.Environment;
+//import android.support.constraint.ConstraintLayout;
+//import android.support.design.widget.CoordinatorLayout;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -45,6 +49,8 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.android.volley.RequestQueue;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
